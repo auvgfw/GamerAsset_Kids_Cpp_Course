a0be9e9b88cb755178cdf95055e999ddc050f592
Zou Lance <double_again@hotmail.com>
2021-01-04 08:36:38 +0800

    增加逆序斐波那契数列范例

---
+++ L4_ReverseFibonacci.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[20];
+    int i;
+    a[0]=0;
+    a[1]=1;
+    for(i=2;i<20;i++)
+    {
+        a[i]=a[i-2]+a[i-1];
+    }
+    for(i=19;i>=0;i--)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
