8bea39ad94eb2b92b4871bf5d612d3d445dae494
ZouLance <double_again@hotmail.com>
2021-01-07 02:54:11 +0800

    更新离散化编程范例

--- lab.cpp
+++ lab.cpp
@@ -1,31 +1,45 @@
-#include <iostream>
-using namespace std;
-int DrawSquare(char x, int n)
+#include "stdio.h"
+struct num
 {
-    int i;
-    for (i = 1; i <= n; i++)
-    {
-        cout << x;
-    }
-    cout << endl;
-    for (i = 1; i <= n - 2; i++)
-    {
-        cout << x;
-        for (int j = 1; j <= n - 2; j++)
-        {
-            cout << " ";
-        }
-        cout << x;
-        cout << endl;
-    }
-
-    for (i = 1; i <= n; i++)
-    {
-        cout << x;
-    }
-    return 0;
-}
+    int data;
+    int data2;
+    int rank;
+};
 int main()
 {
-    DrawSquare('c', 5);
+    int n, i, j, k;
+    int temp = 1;
+    scanf("%d", &n);
+    struct num a[10000];
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i].data);
+        a[i].data2 = a[i].data;
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[i].data2 == a[j].data2)
+            {
+                a[j].data2 = -1;
+            }
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (a[i].data > a[j].data && a[j].data2 != -1)
+            {
+                temp++;
+            }
+        }
+        a[i].rank = temp;
+        temp = 1;
+    }
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", a[i].rank);
+    }
 }
